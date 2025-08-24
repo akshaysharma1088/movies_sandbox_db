63179efd09a412e56032a693496ebb74d8a833e5
akshaysharma1088 <akshay.sharma1088@gmail.com>
2025-08-24 01:01:00 -0500

    Add files via upload

---
+++ revenue_by_genre_by_year.sql
@@ -0,0 +1,14 @@
+SELECT
+    EXTRACT(YEAR FROM M.release_date) AS release_year,
+    G.name AS genre_name,
+    SUM(M.revenue) AS total_revenue
+FROM
+    movies M
+JOIN
+    movie_genres MG ON M.movie_id = MG.movie_id
+JOIN
+    genres G ON MG.genre_id = G.genre_id
+GROUP BY
+    release_year, genre_name
+ORDER BY
+    release_year, total_revenue DESC;
